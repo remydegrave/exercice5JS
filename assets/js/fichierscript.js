3d84c81a583d6bdd96ab358000b2f5a4f93288f0
remydegrave <bolieve.fr@gmail.com>
2020-11-03 11:44:57 +0100

    modification

--- assets/js/fichierscript.js
+++ assets/js/fichierscript.js
@@ -2,7 +2,7 @@ document.getElementById('clickButton').onclick  =showAlert;
 function showAlert(){
     var dividende = document.getElementById('dividende').Value;
     var diviseur = document.getElementById('diviseur').Value;
-    var result = (dividende%diviseur)
+    var result = (dividende%diviseur);
 
-    alert('reste de la division :' +result)
+    alert('reste de la division :' +result);
 }
